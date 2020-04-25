30f5b2bc88c75987b893ba351207bc8acc43a305
sohammain <sohamnaik18@gmail.com>
2020-04-24 18:08:50 -0600

    Modified 201. Bitwise AND of Numbers Range

--- 201. Bitwise AND of Numbers Range.cpp
+++ 201. Bitwise AND of Numbers Range.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-    // Time: O(1), Space: O(1)
+    // Time: O(log(n)), Space: O(1)
     int rangeBitwiseAnd(int m, int n) {
         long diff = n-m, num = 1;
         while(num<diff)
-            num *= 2;
+            num <<= 1;
         
-        return n&(~int(num-1)&m);
+        return (m&n)&~int(num-1);
     }
 };
