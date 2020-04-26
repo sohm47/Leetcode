8eebe36ba637467fa3df47da15bf531118681910
sohammain <sohamnaik18@gmail.com>
2020-04-25 19:34:42 -0600

    Modified 55. Jump Game

--- 55. Jump Game.cpp
+++ 55. Jump Game.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     // Time: O(N), Space: O(1)
     bool canJump(vector<int>& nums) {
-        int jump = 0;
+        int jump = 0, LEN = nums.size()-1;
         for(int i=0; i<nums.size(); i++) {
-            if(jump == 0 && nums[i] == 0 && i != nums.size()-1)
+            if(jump == 0 && nums[i] == 0 && i != LEN)
                 return false;
             if(nums[i] > jump)
-                jump += nums[i]-jump;
+                jump = nums[i];
             --jump;
         }
         
