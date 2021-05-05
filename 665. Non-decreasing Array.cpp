d4c909103d6952631497d72923d42cf8126753e0
soham47 <sohamnaik18@gmail.com>
2021-05-04 21:47:52 -0600

    Modified 665. Non-decreasing Array

--- 665. Non-decreasing Array.cpp
+++ 665. Non-decreasing Array.cpp
@@ -3,14 +3,15 @@ public:
     // Time: O(N), Space: O(1)
     bool checkPossibility(vector<int>& nums) {
         int count = 0;
-        for(int i=1; i<nums.size(); i++) {
-            if(nums[i]<nums[i-1]) {
-                if(i>1 && nums[i]<nums[i-2] && i+1 != nums.size() && nums[i+1]<nums[i-1])
-                    return false;
-                else if(++count > 1)
-                    return false;
+        
+        for(int i=1; i<nums.size() && count < 2; i++) {
+            if(nums[i-1] > nums[i]) {
+                ++count;
+                if(i-2 > -1 && nums[i-2]>nums[i])
+                    nums[i] = nums[i-1];
             }
         }
-        return true;
+        
+        return count < 2;
     }
 };
