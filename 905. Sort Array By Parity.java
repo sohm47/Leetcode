6717cc84ab73084e53d493c00dda9c17fe5a0209
soham47 <sohamnaik18@gmail.com>
2021-09-29 19:45:57 -0600

    Modified 905. Sort Array By Parity

--- 905. Sort Array By Parity.java
+++ 905. Sort Array By Parity.java
@@ -1,16 +1,22 @@
 class Solution {
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    
     // Time: O(N)
-    // Space: O(1)
+    // Space: O(N)
     public int[] sortArrayByParity(int[] nums) {
-        int slowPointer = 0, fastPointer = 0;
-        while(fastPointer < nums.length) {
-            if(nums[fastPointer]%2 == 0) {
-                int temp = nums[slowPointer];
-                nums[slowPointer] = nums[fastPointer];
-                nums[fastPointer] = temp;
-                slowPointer += 1;
-            }
-            fastPointer += 1;
+        int left = 0, right = nums.length-1;
+        while(left < right) {
+            if(nums[left]%2 > nums[right]%2)
+                swap(nums, left, right);
+            
+            if(nums[left]%2 == 0)
+                left += 1;
+            if(nums[right]%2 != 0)
+                right -= 1; 
         }
         
         return nums;
