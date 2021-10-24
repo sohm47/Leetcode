beeea52be193b7978fb39db4141a5bb733c904e8
sohm47 <sohamnaik18@gmail.com>
2021-10-24 15:36:58 -0600

    Added 2043. Simple Bank System

---
+++ 2043. Simple Bank System.java
@@ -0,0 +1,35 @@
+class Bank {
+    long[] balance;
+    
+    public Bank(long[] balance) {
+        this.balance = balance;
+    }
+    
+    public boolean transfer(int account1, int account2, long money) {
+        return account2 <= balance.length && withdraw(account1, money) && deposit(account2, money);
+    }
+    
+    public boolean deposit(int account, long money) {
+        if(account > balance.length)
+            return false;
+        
+        balance[account-1] += money;
+        return true;
+    }
+    
+    public boolean withdraw(int account, long money) {
+        if(account > balance.length || balance[account-1] < money)
+            return false;
+        
+        balance[account-1] -= money;
+        return true;
+    }
+}
+
+/**
+ * Your Bank object will be instantiated and called as such:
+ * Bank obj = new Bank(balance);
+ * boolean param_1 = obj.transfer(account1,account2,money);
+ * boolean param_2 = obj.deposit(account,money);
+ * boolean param_3 = obj.withdraw(account,money);
+ */
