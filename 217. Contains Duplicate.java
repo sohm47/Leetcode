4b379c354652e4ab740487bfcdb50b2f85ed9f4b
Soham Naik <sohamnaik18@gmail.com>
2022-01-30 01:01:57 -0700

    Added 217. Contains Duplicates

---
+++ 217. Contains Duplicate.java
@@ -0,0 +1,15 @@
+class Solution {
+    // Time: O(N), Space: O(N)
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int i=0; i<nums.length; i++) {
+            if(set.contains(nums[i])) {
+                return true;
+            }
+            set.add(nums[i]);
+        }
+        
+        return false;
+    }
+}
