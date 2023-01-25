c5aa1d9974512f327d59de157b6edd8bbe94b939
Soham Naik <sohamnaik18@gmail.com>
2023-01-24 21:58:43 -0800

    Added 122. Best Time to Buy and Sell Stock II

---
+++ 122. Best Time to Buy and Sell Stock II.java
@@ -0,0 +1,13 @@
+class Solution {
+    // Time: O(N), Space: O(1)
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        for(int i=1; i<prices.length; i++) {
+            if(prices[i] > prices[i-1]) {
+                profit += (prices[i] - prices[i-1]);
+            }
+        }
+
+        return profit;
+    }
+}
